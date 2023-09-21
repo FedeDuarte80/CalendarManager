9023fd3eabae968f01bf1b6f45f6bb02df9197ff
FedeDuarte80 <duser80@hotmail.com>
2023-09-22 01:22:30 +0200

    Initial Commit

---
+++ CalendarManager/CalendarManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarManagerApp.swift
+//  CalendarManager
+//
+//  Created by Fede Duarte on 22/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CalendarManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarManager/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CalendarManager
+//
+//  Created by Fede Duarte on 22/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
